63e6c534adb168ca5be7c3766c9c18590eebee9a
Ken Fleming <kfleming1@yahoo.com>
2014-12-07 17:31:00 -0500

    Project 1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+subset1 <- subset(data, data$Date == "2/2/2007" | data$Date == "1/2/2007")
+with(subset1, hist(Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)"))
+dev.copy(png, file="plot1.png", width = 480, height = 480)
+dev.off()
